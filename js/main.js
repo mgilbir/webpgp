73c7e92c2df6dcc970b5f984209cfba7a48b90e7
Miguel Eduardo Gil Biraud <m.gilbiraud@minddistrict.com>
2018-02-12 16:04:34 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,92 @@
+var clipboard = new Clipboard('.copy_btn');
+
+// clipboard.on('success', function(e) {
+//     $(".copy_btn").tooltip({
+//         delay: { "show": 500, "hide": 0 },
+//         "title": "Copied!",
+//         "once": true,
+//     })
+//     e.clearSelection();
+// });
+
+function generateKey() {
+    //Set input as readOnly
+    document.getElementById("name").readOnly = true;
+    document.getElementById("email").readOnly = true;
+    document.getElementById("passphrase").readOnly = true;
+
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var passphrase = document.getElementById("passphrase").value;
+
+    var options = {
+        userIds: [{ name: name, email: email }], // multiple user IDs
+        numBits: 4096,                                            // RSA key size
+        passphrase: passphrase // protects the private key
+    };
+
+    console.log(options);
+
+    document.getElementById("generating_button").hidden = true;
+    document.getElementById("generating_spinner").hidden = false;
+    
+    openpgp.generateKey(options).then(function(key) {
+        var privkey = key.privateKeyArmored; // '-----BEGIN PGP PRIVATE KEY BLOCK ... '
+        var pubkey = key.publicKeyArmored;   // '-----BEGIN PGP PUBLIC KEY BLOCK ... '
+
+        document.getElementById("generated_private_key").value = privkey;
+        document.getElementById("generated_public_key").value = pubkey;
+        $(".collapse").collapse('show');
+
+        document.getElementById("generating_spinner").hidden = true;
+
+    }).catch(function(e) {
+        reset_key_generation_state();
+        console.log(e)
+    });
+}
+
+function reset_key_generation_state() {
+    //Set input as readOnly
+    document.getElementById("name").readOnly = false;
+    document.getElementById("email").readOnly = false;
+    document.getElementById("passphrase").readOnly = false;
+
+    document.getElementById("generating_button").hidden = false;
+    document.getElementById("generating_spinner").hidden = true;
+}
+
+function decrypt() {
+    var privkey = document.getElementById("private_key").value
+    var passphrase = document.getElementById("passphrase").value
+    
+    var privKeyObj = openpgp.key.readArmored(privkey).keys[0];
+
+    privKeyObj.decrypt(passphrase);
+
+    var oReq = new XMLHttpRequest();
+    
+    oReq.onload = function(e) {
+        var encryptedText = oReq.responseText; // not responseText
+        console.log(encryptedText)
+
+        options = {
+            message: openpgp.message.readArmored(encryptedText),     // parse armored message
+            // publicKeys: openpgp.key.readArmored(pubkey).keys,    // for verification (optional)
+            privateKey: privKeyObj // for decryption
+        };
+        
+        openpgp.decrypt(options).then(function(plaintext) {
+            // document.getElementById("result").innerHTML = plaintext.data;
+            console.log(plaintext.data)
+            var blob = new Blob([plaintext.data], {type: "text/plain;charset=utf-8"});
+            saveAs(blob, "hello_world.txt");
+        });    
+
+    }
+    
+    var url = document.getElementById("encrypted_url").value
+
+    oReq.open("GET", url);
+    oReq.send();
+}
